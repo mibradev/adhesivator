21fd9d834a7c3de3118a92baca0f4fc230810422
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2024-11-29 17:02:46 +0200

    Fix root link

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
     <html dir="rtl" lang="ar">
       <body className="bg-slate-800 text-slate-200">
         <h1 className="bg-slate-900 p-8 text-center text-xl font-bold text-slate-400">
-          <a href="/">Adhesivator</a>
+          <Link href="/">Adhesivator</Link>
         </h1>
         <main className="mx-auto my-8 w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12 2xl:w-2/12">
           {children}
