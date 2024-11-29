b8ca757716fe1b334e9b981cc7718bf0434e5c94
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2024-11-29 14:49:01 +0200

    Update Tailwind CSS

--- app/components.js
+++ app/components.js
@@ -1,7 +1,7 @@
 export function Input(props) {
   return (
     <input
-      className="block px-2 py-1 w-full rounded bg-slate-600 text-slate-200"
+      className="block px-2 py-1 w-full rounded-sm bg-slate-600 text-slate-200"
       {...props}
     />
   );
@@ -14,7 +14,7 @@ export function InputNumber(props) {
 export function InputResult(props) {
   return (
     <InputNumber
-      className="block p-2 w-full rounded bg-emerald-600 text-white text-center font-bold"
+      className="block p-2 w-full rounded-sm bg-emerald-600 text-white text-center font-bold"
       readOnly
       {...props}
     />
@@ -24,7 +24,7 @@ export function InputResult(props) {
 export function InputResultSecondary(props) {
   return (
     <InputNumber
-      className="block px-2 py-1 w-full rounded bg-slate-700 text-slate-200 text-center"
+      className="block px-2 py-1 w-full rounded-sm bg-slate-700 text-slate-200 text-center"
       readOnly
       {...props}
     />
--- app/page.js
+++ app/page.js
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className="space-y-4">
       <a
-        className="block p-8 rounded bg-slate-700 text-slate-300 text-center text-lg"
+        className="block p-8 rounded-sm bg-slate-700 text-slate-300 text-center text-lg"
         href="by-length"
       >
         الحساب بطول البكرة
       </a>
       <a
-        className="block p-8 rounded bg-slate-700 text-slate-300 text-center text-lg"
+        className="block p-8 rounded-sm bg-slate-700 text-slate-300 text-center text-lg"
         href="by-weight"
       >
         الحساب بوزن العمود
